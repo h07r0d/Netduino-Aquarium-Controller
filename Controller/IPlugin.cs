182523e5ffde42d98000001553d124f850c2a8ce
Todd McDonald <h07r0d@gmail.com>
2012-02-19 20:07:53 -0500

    Relay Shield control code.
    Added ControlType plugin

--- Controller/IPlugin.cs
+++ Controller/IPlugin.cs
@@ -1,8 +1,21 @@
 using System;
+using System.Collections;
 
 namespace Controller
 {
 	public enum ThingSpeakFields : uint { Temperature = 1, pH = 2 };
+
+	public struct RelayCommand
+	{
+		public int relay;
+		public bool status;
+
+		public RelayCommand(int _relay, bool _status)
+		{
+			this.relay = _relay;
+			this.status = _status;
+		}
+	}
 
 	public abstract class Plugin : IDisposable
 	{
@@ -18,6 +31,12 @@ namespace Controller
 	public abstract class OutputPlugin : Plugin 
 	{
 		public abstract void EventHandler(Object sender, IPluginData data);		
+	}
+
+	public abstract class ControlPlugin : Plugin
+	{
+		public abstract DictionaryEntry ParseCommand(String time, String command);
+		public abstract void ExecuteControl(Object state);
 	}
 
 	public interface IPluginData
---
+++ Controller/IPluginData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.SPOT;
+
+namespace Controller
+{
+    public interface IPluginData
+    {
+		ThingSpeakFields DataType();
+		string DataUnits();
+		float GetValue();
+    }
+}
