5c0c4e72b82b1a190343ce845204baf9a607c8eb
JJS <joshs85@gmail.com>
2012-12-11 16:32:14 -0500

    Added Plugin for Digital EC Probe

---
+++ Plugin-ElectricalConductivity/ElectricalConductivity.js
@@ -0,0 +1,13 @@
+﻿var ElectricalConductivityInit = function () {
+    var pluginEnabled = (config.config.input.ElectricalConductivity.enabled === 'true');
+    var ElectricalConductivityEnable = $('#PElectricalConductivityE');
+	ElectricalConductivityEnable.attr('checked', pluginEnabled);
+	ElectricalConductivityEnable.click(function () {
+		$(this).button("option", "label", this.checked ? "Enabled" : "Disabled");
+		config.config.input.ElectricalConductivity.enabled = this.checked ? 'true' : 'false';
+	});
+	ElectricalConductivityEnable.button({ label: (pluginEnabled ? "Enabled" : "Disabled") }).button('refresh');
+	$("#ElectricalConductivityH").val(config.config.input.ElectricalConductivity.interval[0]);
+	$("#ElectricalConductivityM").val(config.config.input.ElectricalConductivity.interval[1]);
+	$("#ElectricalConductivityS").val(config.config.input.ElectricalConductivity.interval[2]);
+};
